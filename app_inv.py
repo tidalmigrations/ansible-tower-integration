898e78649e4eec712ee6252f176de4d7dfc8f302
Phil Rees <philrees@gmail.com>
2016-09-21 13:20:54 -0400

    Add group and tag filtering to script

--- app_inv.py
+++ app_inv.py
@@ -7,13 +7,6 @@ import json
 import yaml
 import argparse
 
-APP_INV_DOMAIN = os.environ['APP_INV_DOMAIN']
-APP_INV_EMAIL = os.environ['APP_INV_EMAIL']
-APP_INV_PASSWORD = os.environ['APP_INV_PASSWORD']
-
-API_PREFIX = "/api/"
-url = "http://" + APP_INV_DOMAIN + API_PREFIX
-
 def fail(msg):
     sys.stderr.write("%s\n" % msg)
     sys.exit(1)
@@ -24,44 +17,57 @@ class ApplicationInventory(object):
         self.args = self._parse_cli_args()
 
     def run(self):
+        self._get_env_vars()
         self._args = self._parse_cli_args()
         self.config_file = self._parse_config_file()
+        self._set_default_config()
+
         ApplicationInventory.cookie = self.login()
-        if self._args.host is None:
+
+        if self.groups is not None:
+            output = self.get_group_servers()
+        elif self._args.host is None:
             servers = self.get_servers()
+            output = self.generate_data(servers)
         else:
             servers = self.get_server()
+            output = self.generate_data(servers)
 
-        output = self.generate_data(servers)
         self.output_data(output)
+
+    def _get_env_vars(self):
+        API_PREFIX = "/api/"
+        self.config_path = os.environ.get('CONFIG_PATH')
+        email = os.environ['APP_INV_EMAIL']
+        password = os.environ['APP_INV_PASSWORD']
+        domain = os.environ.get('APP_INV_DOMAIN')
+        if domain and email and password:
+            self.email = email
+            self.domain = domain
+            self.password = password
+            self.api_url = "https://" + domain + API_PREFIX
+        else:
+            fail("You must provice three environment variables: APP_INV_EMAIL APP_INV_PASSWORD and APP_INV_DOMAIN. The value found for these thee variables was APP_INV_EMAIL: '%s' APP_INV_PASSWORD '%s' and APP_INV_DOMAIN '%s'" % (email, password, domain))
 
     def _parse_config_file(self):
         config = dict()
-        config_path = None
-
-        if self._args.config_file:
-            config_path = self._args.config_file
-        elif self._env_args.config_file:
-            config_path = self._env_args.config_file
-
-        if config_path:
-            try:
-                config_file = os.path.abspath(config_path)
-            except:
-                config_file = None
-
-            if config_file and os.path.exists(config_file):
-                with open(config_file) as f:
+        if self.config_path:
+            if self.config_path and os.path.exists(self.config_path):
+                with open(self.config_path) as f:
                     try:
                         config = yaml.safe_load(f.read())
                     except Exception as exc:
-                        fail("Error: parsing %s - %s" % (config_path, str(exc)))
+                        fail("Error: parsing %s - %s" % (self.config_path, str(exc)))
         return config
 
-    def _parse_cli_args(self):
-        basename = os.path.splitext(os.path.basename(__file__))[0]
-        default_config = basename + '.yml'
+    def _set_default_config(self):
+        # Set the defaults for the config here. The defaults are used when there is no config file
+        # or when there is no matching parameter in the config file.
+        self.property = self.config_file.get("property", "fqdn")
+        self.filter_tags = self.config_file.get("filter-tags", None)
+        self.groups = self.config_file.get("groups", None)
 
+    def _parse_cli_args(self):
         parser = argparse.ArgumentParser(
                 description='Return Ansible inventory for one or more hosts via Application Inventory API')
         parser.add_argument('--list', action='store_true', default=True,
@@ -70,32 +76,84 @@ class ApplicationInventory(object):
                             help='Only get information for a specific host.')
         parser.add_argument('--pretty', action='store_true', default=False,
                            help='Pretty print JSON output(default: False)')
-        parser.add_argument('--config-file', action='store', default=default_config,
-                            help="Name of the config file to use. Default is %s" % (default_config))
         return parser.parse_args()
 
     def login(self):
-        login_uri = url + "login"
-        body = {"email": APP_INV_EMAIL, "password": APP_INV_PASSWORD}
+        login_uri = self.api_url + "login"
+        body = {"email": self.email, "password": self.password}
         r = requests.post(login_uri, data = body)
         return r.cookies
 
     def get_servers(self):
-        s = requests.get(url + "servers", cookies = self.cookie)
-        return s.json()
+        if self.filter_tags and self.filter_tags["tags"]:
+            tags = []
+            for t in self.filter_tags["tags"]:
+                tags.append(self.get_tag(t))
+            ids = ",".join(str(t) for t in tags)
+            params = {"tag_ids":ids}
+            if self.filter_tags["logic"]:
+                params["query_type"] = self.filter_tags["logic"]
+            else:
+                params["query_type"] = "All"
+            s = requests.get(self.api_url + "servers", params = params, cookies = self.cookie)
+        else:
+            s = requests.get(self.api_url + "servers", cookies = self.cookie)
+        return s.json()[:100]
 
     def get_server(self):
         data = self.get_servers()
-        data = filter(lambda s: s[self.config_file["parsing"]["property"]] == self._args.host, data)
+        data = filter(lambda s: s[self.property] == self._args.host, data)
         return data
+
+    def get_group_servers(self):
+        groups = self.get_tags()
+        data = {}
+        for g, props in groups.iteritems():
+            tag_ids = ",".join([str(t["id"]) for t in props["tags"]])
+            params = {"query_type": props["logic"], "tag_ids": tag_ids}
+            s = requests.get(self.api_url + "servers", params = params, cookies = self.cookie)
+            data[g] = s.json()
+        hostvars = {}
+        dev = 0
+        for group, servers in data.iteritems():
+            data[group] = []
+            for s in servers:
+                if s[self.property] is not None:
+                    data[group].append(s[self.property])
+                    hostvars[s[self.property]] = s
+                dev += 1
+                if dev > 98:
+                    break
+
+        response = data
+        response["_meta"] = {"hostvars": hostvars}
+        return response
+
+    def get_tags(self):
+        groups = {}
+        for g, props in self.groups.iteritems():
+            tags = []
+            groups[g]= {}
+            for t in props["tags"]:
+                tags.append({"name": t, "id": self.get_tag(t)})
+            groups[g]["tags"] = tags
+            groups[g]["logic"] = props["logic"]
+        return groups
+
+    def get_tag(self, tag):
+        r = requests.get(self.api_url + "tags", params = {"search" : tag}, cookies = self.cookie)
+        return r.json()[0]["id"]
 
     def generate_data(self, data):
         if type(data) is list:
             servers = []
+            hostvars = {}
             for s in data:
-                if s[self.config_file["parsing"]["property"]] is not None:
-                    servers.append(s[self.config_file["parsing"]["property"]])
-            return {"servers":servers, "_meta":{"hostvars":servers}}
+                if s[self.property] is not None:
+                    servers.append(s[self.property])
+                    hostvars[s[self.property]] = s
+            return {"servers":{"hosts":servers}, "_meta": {"hostvars": hostvars}}
+
         else:
             return data
 
